4eb0b6fb6f4c8b47c1382d6f5a7714113ed6639a
alanaraujo-bit <alanvitoraraujo2a@gmail.com>
2026-08-08 06:25:55 -0300

    fix: preserva checksum da migracao inicial

--- servidor/migracoes/0001_contas.sql
+++ servidor/migracoes/0001_contas.sql
@@ -25,7 +25,6 @@ CREATE TABLE atalhos (
     conta_id  TEXT NOT NULL REFERENCES contas(id) ON DELETE CASCADE,
     codigo    TEXT NOT NULL,
     nome      TEXT NOT NULL,
-    foto      TEXT NOT NULL DEFAULT '',
     PRIMARY KEY (conta_id, codigo)
 );
 
